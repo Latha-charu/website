bd7d839aed165910347f7d1f6085067dd0ba6d34
Latha-charu <71610564+Latha-charu@users.noreply.github.com>
2022-01-04 17:22:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const bars = document.querySelector('.bars');
 const block = document.querySelector('.smallscreen1');
 const close = document.querySelector('.close');
+const navigator = document.querySelector('.navigation');
+const navheight = navigator.getBoundingClientRect().height;
 
+
+// making sticky the navigator
+window.addEventListener('scroll', ()=>{
+    // console.log(navheight);
+    const scrollheight= window.pageYOffset;
+    // console.log(scrollheight);
+    if(scrollheight>navheight){
+        navigator.classList.add('fixed-nav');
+    }else{
+        navigator.classList.remove('fixed-nav');
+    }
+
+})
+
+// closing and opening bars and close button
 
 bars.addEventListener('click', () =>{
  block.style.display = 'block';
